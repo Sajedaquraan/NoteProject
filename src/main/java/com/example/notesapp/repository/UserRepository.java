3679a62bc731986b97b1f1005586c07348089718
big0soft <131809846+big0soft@users.noreply.github.com>
2025-02-01 18:29:29 +0300

    Update UserRepository.java

--- src/main/java/com/example/notesapp/repository/UserRepository.java
+++ src/main/java/com/example/notesapp/repository/UserRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import java.util.List;
 import java.util.Optional;
-
+// todo annotated with @Repository
 public interface UserRepository extends JpaRepository<Users, Long> {
 
     List<Users> findByUserIdentifier(String userIdentifier);
